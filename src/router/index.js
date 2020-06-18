ba2784f8bc3d77dd09d3dbda395a5332d24f82a8
sensitiveMix <m18205253820@163.com>
2020-06-18 17:49:19 +0800

    feat(deatil): add detail banner

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/home/Home.vue'
+import Home from '@/views/home/Home'
 import City from '@/views/city/City'
+import Detail from '@/views/detail/Detail'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/city',
     name: 'City',
     component: City
+  },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail
   }
 ]
 
---
+++ src/views/detail/components/Banner.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div class="banner">
+            <img src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" class="banner-img">
+            <div class="banner-info">
+                <div class="banner-title">
+                    大连圣亚海洋世界(AAAA景区)
+                </div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe6e5;</span>
+                    22
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner
+        position relative
+        height 0
+        padding-bottom 55%
+        .banner-img
+            width 100%
+        .banner-info
+            display flex
+            position absolute
+            line-height .6rem
+            height .6rem
+            bottom 0
+            left 0
+            right 0
+            color #fff
+            background-image: -webkit-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+            .banner-title
+                flex 1
+                font-size .32rem
+                padding 0 .2rem
+            .banner-number
+                height .32rem
+                line-height .32rem
+                margin-top .14rem
+                padding 0 .4rem
+                font-size .24rem
+                background rgba(0, 0, 0, .8)
+                border-radius .2rem
+                .banner-icon
+                    font-size .24rem
+</style>
