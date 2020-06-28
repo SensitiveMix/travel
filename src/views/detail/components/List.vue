9d70b051800543a17497575d91f2becb732aa44a
sensitiveMix <m18205253820@163.com>
2020-06-28 13:20:37 +0800

    feat(detail): add detail list

---
+++ src/views/detail/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <div class="list" v-for="item of list" :key=item.title>
+        <span>> {{item.title}}</span>
+        <detail-list :list="item.children" class="list-children"></detail-list>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .list
+    padding-top .2rem
+    font-size .28rem
+    line-height .3rem
+    .list-children
+      padding-left .4rem
+</style>
