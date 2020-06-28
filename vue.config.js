5702fee85c58902e2a83e3bbc52e4ff3f02acb03
sensitiveMix <m18205253820@163.com>
2020-06-28 13:03:06 +0800

    chore: remove console log

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
-// const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 
 module.exports = {
   chainWebpack: config => {
